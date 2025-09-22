815955569aac57751f857defdafde7dddcf4f1ca
DEVthiagoSantos <thiagonascimentodossantos44@gmail.com>
2025-09-22 17:14:34 -0300

    Aqui está, não terminei ainda, meu dia nem sempre me da o luxo de ficar horas no notebook, mas sempre que possivel eu estou praticando

---
+++ src/Cadastro.java
@@ -0,0 +1,36 @@
+public class Cadastro {
+
+    private String nome;
+    private int matricula;
+    private String curso;
+
+    public Cadastro(String nome, int matricula, String curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Cadastro> lista = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        String menu = """
+                ----------------------------
+                [    Cadastro de Alunos    ]
+                [ 1 ] Cadastrar
+                [ 2 ] Buscar
+                [ 3 ] Exibir lista de alunos
+                [ 4 ] Sair
+                """;
+
+        while (true) {
+
+            System.out.println(menu);
+            System.out.print("Escolha: ");
+            int opcao = input.nextInt();
+            input.nextLine();
+
+            if (opcao == 1) {
+                System.out.print("Nome do Aluno: ");
+                String nome = input.nextLine();
+                int matricula = 0;
+                boolean encontrado = false;
+                for (Cadastro cadastro : lista) {
+                    while (!encontrado) {
+                        System.out.print("Numero de Matricula: ");
+                        int num = input.nextInt();
+                        input.nextLine();
+                        if (cadastro.getMatricula() == num) {
+                            System.out.println("Já existe um usuario com esse numero de matricula");
+                        } else {
+                            matricula = num;
+                            encontrado = true;
+                            break;
+                        }
+                    }
+                }
+                System.out.print("Nome do Curso: ");
+                String curso = input.nextLine();
+                lista.add(new Cadastro(nome, matricula, curso));
+                System.out.println("Ok, cadastrado com sucesso");
+            } else if (opcao == 2) {
+
+                System.out.print("Certo, digite o numero de matricula: ");
+                int numero = input.nextInt();
+                boolean encontrado = false;
+                for (Cadastro cadastro : lista) {
+
+                    if (cadastro.getMatricula() == numero) {
+                        System.out.println("Nome: " + cadastro.getNome());
+                        System.out.println("Curso: " + cadastro.getCurso());
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    System.out.println("Esse usuario não existe");
+                }
+
+            } else if (opcao == 4) {
+                break;
+            }
         }
+
     }
+
 }
