8b89dd1812c3fcdce1fd48e526274aa447a3ebe4
DEVthiagoSantos <thiagonascimentodossantos44@gmail.com>
2025-09-22 18:05:51 -0300

    Finalizei as opções, mas ainda to pensando em automatizar o numero da matricula, tirar a opção de adicionar

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         String menu = """
-                ----------------------------
                 [    Cadastro de Alunos    ]
                 [ 1 ] Cadastrar
                 [ 2 ] Buscar
                 [ 3 ] Exibir lista de alunos
                 [ 4 ] Sair
+                ----------------------------
                 """;
 
         while (true) {
@@ -27,15 +27,16 @@ public class Main {
             if (opcao == 1) {
                 System.out.print("Nome do Aluno: ");
                 String nome = input.nextLine();
-                int matricula = 0;
+                int matricula = 1;
                 boolean encontrado = false;
                 for (Cadastro cadastro : lista) {
                     while (!encontrado) {
                         System.out.print("Numero de Matricula: ");
                         int num = input.nextInt();
                         input.nextLine();
-                        if (cadastro.getMatricula() == num) {
-                            System.out.println("Já existe um usuario com esse numero de matricula");
+                        if (cadastro.getMatricula() == num || num == 0) {
+                            System.out.println("Verifique se alguém já tem esse numero de matricula");
+                            System.out.println("OBS: 0 Não é permitido");
                         } else {
                             matricula = num;
                             encontrado = true;
@@ -47,6 +48,7 @@ public class Main {
                 String curso = input.nextLine();
                 lista.add(new Cadastro(nome, matricula, curso));
                 System.out.println("Ok, cadastrado com sucesso");
+                System.out.println("----------------------------");
             } else if (opcao == 2) {
 
                 System.out.print("Certo, digite o numero de matricula: ");
@@ -57,16 +59,35 @@ public class Main {
                     if (cadastro.getMatricula() == numero) {
                         System.out.println("Nome: " + cadastro.getNome());
                         System.out.println("Curso: " + cadastro.getCurso());
+                        System.out.println("----------------------------");
                         encontrado = true;
                         break;
                     }
                 }
                 if (!encontrado) {
                     System.out.println("Esse usuario não existe");
+                    System.out.println("----------------------------");
                 }
 
-            } else if (opcao == 4) {
+            } else if (opcao == 3) {
+
+                System.out.println("----------- Lista dos Alunos -----------");
+                if (lista.isEmpty()) {
+                    System.out.println("Não tem ninguem da lista");
+                } else {
+                    for (Cadastro cadastro : lista) {
+
+                        System.out.println("Nome: " + cadastro.getNome());
+                        System.out.println("Matricula: " + cadastro.getMatricula());
+                        System.out.println("Curso: " + cadastro.getCurso());
+                        System.out.println("-------------------------------------");
+
+                    }
+                }
+            }else if (opcao == 4) {
                 break;
+            } else {
+                System.out.println("Perdão, essa opção não está disposivel");
             }
         }
 
